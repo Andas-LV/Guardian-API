e9d25b6f2dd7717f1cce9f6446134d54494920c9
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2023-09-26 18:07:07 +0600

    cmt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,36 @@
-import { Provider } from 'react-redux';
-import store from './store/store'; // Импортируйте ваш созданный Redux Store
+'use client'
+import React, { createContext, useContext, ReactNode } from 'react';
+import store from '../store/store';
 
-function MyApp() {
+// Создаем контекст для передачи store
+const ReduxStoreContext = createContext(store);
+// Создаем компонент-обертку для передачи store через контекст
+interface ReduxStoreProviderProps {
+  children: ReactNode;
+}
+
+function ReduxStoreProvider({ children }: ReduxStoreProviderProps) {
   return (
-    <Provider store={store}>
-      ааа
-    </Provider>
+    <ReduxStoreContext.Provider value={store}>
+      {children}
+    </ReduxStoreContext.Provider>
   );
 }
 
-export default MyApp;
+function MyApp() {
+  const reduxStore = useContext(ReduxStoreContext);
+  return (
+    <div>
+      <p>aaa</p>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <ReduxStoreProvider>
+      <MyApp />
+    </ReduxStoreProvider>
+  );
+}
+
