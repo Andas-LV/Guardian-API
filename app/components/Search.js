c1e043c989371605e945623fd09ee6b884c48f82
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2023-10-06 18:48:33 +0600

    go

---
+++ app/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../page.module.css'
+
+function Search() {
+  return (
+    <div className={styles.wrapper}>
+        input
+    </div>
+  )
+}
+export default Search
