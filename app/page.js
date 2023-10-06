f3d3bd4d3a3a9777eca43e1dcd04b98cc9173003
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2023-10-06 18:37:35 +0600

    cmt

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 import styles from "./page.module.css";
-import { Provider } from "react-redux";
-import store from "../store/store"
+
 
 export default function Home() {
-  return (
-    <Provider store={store}>
-      <main className={styles.main}>google</main>
-    </Provider>
-  );
+  return(
+    <main className={styles.main}>
+      google
+    </main>
+  )
 }
